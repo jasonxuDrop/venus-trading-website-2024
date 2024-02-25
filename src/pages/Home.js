8feb22f2424a560326b5f85039247aef7b84fc16
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-02-25 13:52:28 -0800

    Update Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Home = () => {
+
+    return(
+        <div></div>
+    )
+    // header
+
+    // about
+
+    // product list
+
+    // footer
+
+}
+
+export default Home
