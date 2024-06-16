49bc7dbd0ffba40cb98881e379ba69f6c16329ed
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-06-16 15:26:06 -0700

    adjust footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import logo from "../assets/images/logo/Logo Icon@4x 1.png";
 const Footer = () => {
   return (
     <footer className="bg-footerBgColor">
-      <div className="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-8 py-12 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
           <div className="sm:col-span-1 lg:col-span-3">
             <div className="flex items-center flex-shrink-0">
