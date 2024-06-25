cbe5944a5e1c02b1514f07924cf11b4ee2b1375f
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-06-25 16:31:00 -0700

    removed className in About

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,16 +6,16 @@ const About = () => {
     <div className="max-w-screen-xl mx-auto px-8 sm:px-6 lg:px-12">
       <div className="relative grid grid-cols-12 gap-2">
         <div className="col-span-12 lg:col-span-5 text-left border-2 my-auto">
-          <h3 className="text-heading3">
+          <h1>
             Import, Export Experts In Weifang, China
-          </h3>
-          <p className="text-mobilePText mt-2">
+          </h1>
+          <p className="mt-2">
             Weifang SevenUp International Trading Co., Ltd. specializes in the
             import, export, and processing of textiles. Leveraging the abundant
             local textile resources, we maintain a comprehensive and rigorous
             quality management system.
           </p>
-          <p className="text-mobilePText mt-2">
+          <p className="mt-2">
             We export a wide range of high-quality products to Japan, United
             States, Europe, and other regions throughout the year.
           </p>
