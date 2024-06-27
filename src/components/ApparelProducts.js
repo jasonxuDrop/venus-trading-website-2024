3434ea689af6f9eda67b8aa2947b95abcf445870
WoodyXiao <xwd443907076@hotmail.com>
2024-06-27 11:57:17 -0700

    update the layout

--- src/components/ApparelProducts.js
+++ src/components/ApparelProducts.js
@@ -3,7 +3,7 @@ import ProductCard from "./productCard";
 
 const Products = () => {
   return (
-    <div className="max-w-screen-xl mx-auto px-3.5 sm:px-16 lg:px-32 lg:h-[calc(100vh-10rem)]">
+    <div className="max-w-screen-xl mx-auto px-3.5 sm:px-16 lg:px-32 lg:h-[calc(100vh-10rem)] flex flex-col justify-center">
       {/* Apparel Products */}
       <h1 className="text-heading4 text-center mb-[3rem]">Apparel Products</h1>
       <div className="relative grid grid-cols-12 gap-4">
