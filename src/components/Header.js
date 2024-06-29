b7b693e39336658bd3e6983e6f27a01fedf4e33f
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-06-29 11:54:49 -0700

    remove unused thing

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@
  */
 
 import React, { useState, useEffect, useCallback } from "react";
-import { Link } from "react-router-dom";
 import logo from "../assets/images/logo/Logo Icon.png";
 
 const Header = () => {
