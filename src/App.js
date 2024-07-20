7c9fb9091c41cffbc2c3eb84cda85e6dccf44eb6
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-07-20 14:56:18 -0700

    add basename in Router

--- src/App.js
+++ src/App.js
@@ -19,8 +19,10 @@ import './i18n';
 const Home = React.lazy(() => import("./pages/Home"));
 
 function App() {
+  const basename = process.env.NODE_ENV === 'production' ? '/venus-trading-website-2024' : '/';
+
   return (
-    <Router>
+    <Router basename={basename}>
       <div className="App min-h-screen">
         <Header />
         <div className="">
