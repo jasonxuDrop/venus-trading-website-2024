0e89247f06f053f193254e1d86dabb9d80ffd767
WoodyXiao <xwd443907076@hotmail.com>
2024-07-26 23:43:26 -0700

    update about content

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import aboutImg from "../assets/images/aboutImg/pexels-cottonbro-4108797.png";
 import useScreenHeight from "../utils/hooks/useScreenHeight";
 
+import pageImg from "../assets/content/pageImg.json";
+
 const AboutComponent = () => {
+  const { t, i18n } = useTranslation("page");
+
   const smallScreenHeight = useScreenHeight(600);
 
   return (
@@ -10,23 +15,25 @@ const AboutComponent = () => {
       <div className="relative grid grid-cols-12 gap-2">
         <div className="col-span-12 lg:col-span-6 text-left">
           <img
+            // src={t(`about.${pageImg.about.aboutImg.id}`)}
             src={aboutImg}
             alt="About"
-            className={`w-full ${smallScreenHeight ? "" : ""} hidden lg:block object-cover`}
+            className={`w-full ${
+              smallScreenHeight ? "" : ""
+            } hidden lg:block object-cover`}
           />
         </div>
         <div className="col-span-12 lg:col-span-5 lg:col-start-8 text-left my-auto">
-          <h2>Import, Export Experts In Weifang, China</h2>
-          <p className="mt-4">
-            Weifang SevenUp International Trading Co., Ltd. specializes in the
-            import, export, and processing of textiles. Leveraging the abundant
-            local textile resources, we maintain a comprehensive and rigorous
-            quality management system.
-          </p>
-          <p className="mt-4">
-            We export a wide range of high-quality products to Japan, United
-            States, Europe, and other regions throughout the year.
-          </p>
+          <h2>{t(`about.${pageImg.about.title.id}`)}</h2>
+          {
+            pageImg.about.paragraph.map((p)=> {
+              return (
+                <p className="mt-4">{
+                  t(`about.${p.id}`)
+                }</p>
+              )
+            })
+          }
         </div>
       </div>
     </div>
