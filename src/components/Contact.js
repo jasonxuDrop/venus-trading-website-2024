f366ff9be2d58af843c86cab2d5d1fd7817cc7e3
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-09-28 21:03:29 -0700

    update contact form function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,13 +30,21 @@ const Contact = () => {
     if (localStorage.getItem("clearQuoteOnReload") === "true") {
       updateQuoteDetails("");
       localStorage.removeItem("clearQuoteOnReload");
+      setFormData({ name: "", email: "", company: "" });
+      setQuoteTextArea("");
     }
   }, [updateQuoteDetails]);
 
   // Handle the beforeunload event
   useEffect(() => {
     const handleBeforeUnload = (event) => {
-      if (quoteDetails) {
+      if (
+        quoteDetails ||
+        quoteTextArea ||
+        formData.name ||
+        formData.email ||
+        formData.company
+      ) {
         localStorage.setItem("clearQuoteOnReload", "true");
         event.preventDefault();
         event.returnValue = "";
@@ -46,7 +54,7 @@ const Contact = () => {
     window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => window.removeEventListener("beforeunload", handleBeforeUnload);
-  }, [quoteDetails]); // Depend on quoteDetails directly
+  }, [quoteDetails, formData, quoteTextArea]); // Depend on quoteDetails directly
 
   const handleInputChange = (event) => {
     setQuoteTextArea(event.target.value);
@@ -54,12 +62,12 @@ const Contact = () => {
 
   const handleContactInput = (event) => {
     const { name, value } = event.target;
-    console.log('123');
+    console.log("123");
     setFormData({
-        ...formData,
-        [name]: value
+      ...formData,
+      [name]: value,
     });
-};
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -84,6 +92,7 @@ const Contact = () => {
           setFormData({ name: "", email: "", company: "" });
           setQuoteTextArea("");
           alert("Message sent successfully!");
+          updateQuoteDetails("");
         },
         (error) => {
           console.log("Failed to send the email: ", error.text);
@@ -99,108 +108,114 @@ const Contact = () => {
       }`}
     >
       <Reveal>
-      <div className="relative grid grid-cols-12 gap-2">
-        <div className="col-span-12 lg:col-span-5 text-left flex flex-col">
-          <h2 className="mb-[64px]">
-            {t(`contact.${pageImage.contact.context.title}`)}
-          </h2>
-          <p className="mb-[16px]">
-            {t(`contact.${pageImage.contact.context.content}`)}
-          </p>
-          <div className="my-2 mb-[128px]">
-            <p className="text-left flex gap-2">
-              <span className="w-24 flex-none">
-                {t(`contact.${pageImage.contact.context.phone}`)}
-              </span>
-              <span className="">
-                {t(`contact.${pageImage.contact.context.phoneVal}`)}
-              </span>
+        <div className="relative grid grid-cols-12 gap-2">
+          <div className="col-span-12 lg:col-span-5 text-left flex flex-col">
+            <h2 className="mb-[64px]">
+              {t(`contact.${pageImage.contact.context.title}`)}
+            </h2>
+            <p className="mb-[16px]">
+              {t(`contact.${pageImage.contact.context.content}`)}
             </p>
-            <p className="text-left flex gap-2">
-              <span className="w-24 flex-none">
-                {t(`contact.${pageImage.contact.context.fax}`)}
-              </span>
-              <span className="">
-                {t(`contact.${pageImage.contact.context.faxVal}`)}
-              </span>
-            </p>
-            <p className="text-left flex gap-2">
-              <span className="w-24 flex-none">
-                {t(`contact.${pageImage.contact.context.address}`)}
-              </span>
-              <span className="">
-                {t(`contact.${pageImage.contact.context.addressVal}`)}
-              </span>
-            </p>
+            <div className="my-2 mb-[128px]">
+              <p className="text-left flex gap-2">
+                <span className="w-24 flex-none">
+                  {t(`contact.${pageImage.contact.context.phone}`)}
+                </span>
+                <span className="">
+                  {t(`contact.${pageImage.contact.context.phoneVal}`)}
+                </span>
+              </p>
+              <p className="text-left flex gap-2">
+                <span className="w-24 flex-none">
+                  {t(`contact.${pageImage.contact.context.fax}`)}
+                </span>
+                <span className="">
+                  {t(`contact.${pageImage.contact.context.faxVal}`)}
+                </span>
+              </p>
+              <p className="text-left flex gap-2">
+                <span className="w-24 flex-none">
+                  {t(`contact.${pageImage.contact.context.address}`)}
+                </span>
+                <span className="">
+                  {t(`contact.${pageImage.contact.context.addressVal}`)}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="col-span-12 lg:col-span-6 lg:col-start-7 text-left">
+            <form className="space-y-4" onSubmit={handleFormSubmit}>
+              <div className="flex flex-col mb-[16px]">
+                <label htmlFor="name" className="font-semibold text-formColor">
+                  {t(`contact.form.${pageImage.contact.form.name}`)}
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  required
+                  name="name"
+                  className="input-field"
+                  value={formData.name}
+                  onChange={handleContactInput}
+                />
+              </div>
+              <div className="flex flex-col mb-[16px]">
+                <label htmlFor="email" className="font-semibold text-formColor">
+                  {t(`contact.form.${pageImage.contact.form.email}`)}
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  required
+                  name="email"
+                  className="input-field"
+                  onChange={handleContactInput}
+                  value={formData.email}
+                />
+              </div>
+              <div className="flex flex-col mb-[16px]">
+                <label
+                  htmlFor="company"
+                  className="font-semibold text-formColor"
+                >
+                  {t(`contact.form.${pageImage.contact.form.company}`)}
+                </label>
+                <input
+                  type="text"
+                  id="company"
+                  name="company"
+                  className="input-field"
+                  onChange={handleContactInput}
+                  value={formData.company}
+                />
+              </div>
+              <div className="flex flex-col mb-[32px]">
+                <label
+                  htmlFor="message"
+                  className="font-semibold text-formColor"
+                >
+                  {t(`contact.form.${pageImage.contact.form.message}`)}
+                </label>
+                <textarea
+                  id="message"
+                  required
+                  rows="4"
+                  className="resize-y input-field"
+                  value={quoteTextArea}
+                  onChange={handleInputChange}
+                ></textarea>
+              </div>
+              <div className="text-center lg:flex lg:justify-end">
+                <button
+                  type="submit"
+                  className="px-6 py-2 transition-colors duration-150 bg-buttonColor text-black rounded hover:bg-buttonHover"
+                >
+                  {t(`contact.form.${pageImage.contact.form.send}`)}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <div className="col-span-12 lg:col-span-6 lg:col-start-7 text-left">
-          <form className="space-y-4" onSubmit={handleFormSubmit}>
-            <div className="flex flex-col mb-[16px]">
-              <label htmlFor="name" className="font-semibold text-formColor">
-                {t(`contact.form.${pageImage.contact.form.name}`)}
-              </label>
-              <input
-                type="text"
-                id="name"
-                required
-                name="name"
-                className="input-field"
-                value={formData.name}
-                onChange={handleContactInput}
-              />
-            </div>
-            <div className="flex flex-col mb-[16px]">
-              <label htmlFor="email" className="font-semibold text-formColor">
-                {t(`contact.form.${pageImage.contact.form.email}`)}
-              </label>
-              <input
-                type="email"
-                id="email"
-                required
-                name="email"
-                className="input-field"
-                onChange={handleContactInput}
-                value={formData.email}
-              />
-            </div>
-            <div className="flex flex-col mb-[16px]">
-              <label htmlFor="company" className="font-semibold text-formColor">
-                {t(`contact.form.${pageImage.contact.form.company}`)}
-              </label>
-              <input
-                type="text"
-                id="company"
-                name="company"
-                className="input-field"
-                onChange={handleContactInput}
-                value={formData.company}
-              />
-            </div>
-            <div className="flex flex-col mb-[32px]">
-              <label htmlFor="message" className="font-semibold text-formColor">
-                {t(`contact.form.${pageImage.contact.form.message}`)}
-              </label>
-              <textarea
-                id="message"
-                required
-                rows="4"
-                className="resize-y input-field"
-                value={quoteTextArea}
-                onChange={handleInputChange}
-              ></textarea>
-            </div>
-            <div className="text-center lg:flex lg:justify-end">
-              <button
-                type="submit"
-                className="px-6 py-2 transition-colors duration-150 bg-buttonColor text-black rounded hover:bg-buttonHover"
-              >
-                {t(`contact.form.${pageImage.contact.form.send}`)}
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
       </Reveal>
     </div>
   );
