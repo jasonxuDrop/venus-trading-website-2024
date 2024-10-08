8b04d012c49e47185aa041d14eb21b1cc0e4377c
Jason Xu <lemonade3532@outlook.com>
2024-10-07 19:13:23 -0700

    Changed hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import homtImg from "../assets/images/homeImg/hero-image.jpg";
+import homeImg from "../assets/images/homeImg/hero-image.jpg";
 import logo from "../assets/images/logo/Logo Icon@4x 1.png";
 import logoFullCenterEn from "../assets/images/logo/Logo Full EN Center.png";
 import logoNoIconJp from "../assets/images/logo/Logo No Icon JP.svg";
@@ -12,52 +12,53 @@ const Hero = () => {
     return (
         <div
           className="max-w-screen-xl mx-auto pt
-          mt-[-72px]
-          px-3.5
-          sm:px-16 
+          mt-[-72px] px-0 pt-24
+          sm:px-16 sm:pt-36 sm:pb-24
           lg:px-32"
         >
-          <div className="relative grid grid-cols-12 gap-2">
+          <div className="flex flex-col gap-20
+          sm:flex-row sm:justify-between
+          ">
             {/* DESKTOP HERO */}
             <div
-              className="hidden col-span-5 
-            sm:block"
+              className="hidden 
+            sm:block max-w-[38%] flex-none"
             >
               <div className="flex flex-col justify-end h-full">
                 <img
                   src={i18n.language === "en" ? logoNoIconEn : logoNoIconJp}
-                  alt="company logo"
+                  alt="Weifang SevenUp Trading Co., Ltd."
                   className="max-w-full"
                   style={{ marginBottom: "min(12vw, 9rem)" }}
                 />
               </div>
             </div>
-            <div
-              className="hidden col-span-6 col-start-7 object-cover object-bottom
-            sm:block"
-            >
-              <img src={homtImg} alt="" className="" />
-            </div>
 
             {/* MOBILD HERO */}
             <div
-              className="col-span-12 max-w-full h-[calc(1.672*100vw)]
+              className="max-w-full pt-20
             sm:hidden"
             >
-              <div className="flex flex-col items-center justify-center text-center py-16">
+              <div className="flex flex-col items-center justify-center text-center">
                 <img
                   src={logo}
-                  alt="company logo"
+                  alt="Weifang SevenUp Trading Co., Ltd."
                   className="block w-28 h-auto"
                 />
                 {/* TODO: SWITCH TO JP LOGO IS IN JAPANESE */}
                 <img
                   src={logoFullCenterEn}
-                  alt="company logo"
+                  alt="Weifang SevenUp Trading Co., Ltd."
                   className="w-80 max-w-[80vw]"
                 />
               </div>
             </div>
+
+
+            <div className="w-full sm:w-1/2 flex-none">
+              <img src={homeImg} alt="quality bedding product image" className="" />
+            </div>
+
           </div>
         </div>
     )
